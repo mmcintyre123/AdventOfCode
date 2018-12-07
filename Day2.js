b51f97f7e5d9caaf4e60fc9e8b0db69dc7dfbe09
Matthew McIntyre <matthewtfm@gmail.com>
2018-12-07 10:46:15 -0500

    initializing repo

---
+++ Day2.js
@@ -0,0 +1,65 @@
+// var input = [ 'fzostwblnqkhpuzxirnevmaycq' ,'fzastwblgqkupujxirnevmaucd' ,'fzostwbxgakhpujxirnevmayci' ,'uzosmwblgqkhlujxirnevmaycd' ,'bzostwblgqkhpujxirnenfaycd' ,'fzostwblghkhpujxibneemaycd' ,'fzostwblgrkkpujxirnevqaycd' ,'fzostwblgqkhpkjxirqkvmaycd' ,'fzostwblgqdxpujbirnevmaycd' ,'fzostwblgykhfujxirfevmaycd' ,'fzostwbvgskhpujxirnevmagcd' ,'flostwblgokhpuixirnevmaycd' ,'fzostwblgzkhppjxirnevxaycd' ,'ftosuwblgqkhpujxirnevmeycd' ,'fzostkblgqkzpujxirnrvmaycd' ,'fuostwbugqkhnujxirnevmaycd' ,'frostwblgqkhpujsirneamaycd' ,'bzostwblgqkhpajxirnevmuycd' ,'fzysttbluqkhpujxirnevmaycd' ,'czostgblgqkhpujxirnevmgycd' ,'fzostwbcgqkhpujxirnevmavcr' ,'vzostwbdgqrhpujxirnevmaycd' ,'fzostwblgxkhpujxirnekmdycd' ,'fzostwblgdkspzjxirnevmaycd' ,'fzoqtwblgquhpujxirnevkaycd' ,'fzostwblgqjhfujxirnevmayzd' ,'fyostwblgtkhpujxilnevmaycd' ,'izostwblzqkhpupxirnevmaycd' ,'fzoaurblgqkhpujxirnevmaycd' ,'fzostjblgqkhpbjxirbevmaycd' ,'fzostwblgqkhpujmirngvmayca' ,'fxostwbhgqkhiujxirnevmaycd' ,'fzostwblngkhpujxirnemmaycd' ,'fzosgwblgqkhpujxirnlvqaycd' ,'bpostwblgqkhxujxirnevmaycd' ,'fzhstwblgqshpujxirnepmaycd' ,'fzootwbegqkhwujxirnevmaycd' ,'fzmstwblgqkhmujxirnevsaycd' ,'fzostwolgqkhpujxirnevmaysp' ,'foostwblgqohpujxirnevmayyd' ,'fzostwblgqkhgujxgrnevqaycd' ,'fzostwbltqkhjujxipnevmaycd' ,'fsostwblgqkhfujxirnevmayck' ,'fzostwmlgqkbpujxifnevmaycd' ,'fdostwblgqkhsujxirnecmaycd' ,'fzostwblgquhpejxiinevmaycd' ,'fzoqtwblgqkhkujxirnevmaywd' ,'fzostwblmqkmpujxirnyvmaycd' ,'fzjstwblgqkhpuyxirneomaycd' ,'fzortwblgqkhpzjxprnevmaycd' ,'fzosnwulgqkhpujxirnevpaycd' ,'fzostwbegqkvpujxirnevmaykd' ,'fzostwylzqkypujxirnevmaycd' ,'fioshwblgqehpujxirnevmaycd' ,'fzostwbliqkhoujxirnesmaycd' ,'fzostwblgqkhpujdirnelmqycd' ,'fzmstwblgqkhpujxirnnvpaycd' ,'fzostwbtgqkhcujxirnevmaycz' ,'fzostwblgqkfpujeignevmaycd' ,'fzostwbegqkhpuvxirndvmaycd' ,'fzostwblgqkcpujxmrnevmaycp' ,'fvostwblgqkhpyjxirnevmaycx' ,'fzostwblgqkhpufxirnevmzacd' ,'fzisjwblglkhpujxirnevmaycd' ,'fzosowbliqkhgujxirnevmaycd' ,'fzostwblgqkhpujxirnyvmaywl' ,'fzostwbllqnzpujxirnevmaycd' ,'fzostwblgquhpujxirneomayci' ,'fzostwblgqjepujximnevmaycd' ,'fzostwblgqrhbujxijnevmaycd' ,'fzostoblgqkhpujxirnevmoynd' ,'fzortwblgqkhpujxixnevjaycd' ,'fzostwhlgqkapujxirnevmaych' ,'fzostwblgakhpujxirnevfayct' ,'faostwblgqohpljxirnevmaycd' ,'fzostwbhgqkhpujxirnyvcaycd' ,'fjostwblgqkhpwjxirnevjaycd' ,'fzostwblgqklpuixirnevmayxd' ,'fzostwblgqkhhujxirncvmayce' ,'fzostwblzqkhpzjxkrnevmaycd' ,'fzostwblqqkbpujxirnevmcycd' ,'ffostwblxqkhpujxirnevmayxd' ,'fzostwblgqkhpbuxirnefmaycd' ,'szostiblgqkhpujxirnevoaycd' ,'fhlstwblgqkhpujxirnevmaycb' ,'fzostwblgwfhpujxirnevmayed' ,'fyostwblgqkhzujxirnevmayjd' ,'fzostwblgqkvrujxirnevmaucd' ,'fzestwblghkhpujxirnevmaycq' ,'foostwbkgqkhhujxirnevmaycd' ,'fpxstwblgqkhpujxirzevmaycd' ,'fsostwtlgqmhpujxirnevmaycd' ,'feostwelgqkhpumxirnevmaycd' ,'fzostwbvgqkhpujkirnevmayce' ,'fzmsewblgqahpujxirnevmaycd' ,'fzsstsblgkkhpujxirnevmaycd' ,'fzostwblgqkhxajxirneumaycd' ,'fzostwblmqkhpujyisnevmaycd' ,'gzostwblgqkhphjxirnevmavcd' ,'fzostwblgckgpujxirnzvmaycd' ,'qzostdblgqkhpujxirnevmaywd' ,'fzoshwblgskhpufxirnevmaycd' ,'fzosdwblgqkhpujkirnevjaycd' ,'fzvstwblgqkhpuixdrnevmaycd' ,'szostwblgqxhpujeirnevmaycd' ,'fzosvwblgtkhpujxirnevmoycd' ,'fzoscwblgqkkpujxirnevmvycd' ,'fzostsblgqnlpujxirnevmaycd' ,'fzostwblgmkhpujxlrnevjaycd' ,'fzosnwblgqklpugxirnevmaycd' ,'fcostwblgqpmpujxirnevmaycd' ,'ozostwblgbkhpujxirnevmafcd' ,'fzostwbagqkhiujxirneemaycd' ,'fzosxwblgqkhpumxirneymaycd' ,'fpostkblgqkhpujxianevmaycd' ,'fzostwblgvkhhujxirnevmaycl' ,'fzobtwtlgqkhpuwxirnevmaycd' ,'fzostwvwgqkhpujxirtevmaycd' ,'xzostwglgqkhpujtirnevmaycd' ,'fzostwblgvkhtujxirnzvmaycd' ,'jzostwblgqkhpujxirrevmvycd' ,'pzostwbagqkhpujxirnevjaycd' ,'fzostwclgqkhpujxirnhvmeycd' ,'fzostwblglkhpujxirnevmayrf' ,'fzoskwblnqkhpujxirnevmaysd' ,'fzostbblgqkhpujxirnevmjycw' ,'fzostwblggobpujxirnevmaycd' ,'fzostwblgckhpijxirnevmayxd' ,'fiostwrlgqkhpujxirnevmayck' ,'frfstwblvqkhpujxirnevmaycd' ,'fzowvwblgqkhpujsirnevmaycd' ,'fzostwblgquhgujxirnevmiycd' ,'fzoztwblgqkhpujxvrnevmaycj' ,'fzostnbtgqkhpujxixnevmaycd' ,'fzfstwblgjkrpujxirnevmaycd' ,'fzostwblpqkhpdrxirnevmaycd' ,'fivstwblgqkhpuixdrnevmaycd' ,'fzostwbpgqkhpdjxirnewmaycd' ,'fzostwblgqkhpdjxsrngvmaycd' ,'fzostwblsqkhpujxisnxvmaycd' ,'fcosvwblgqkhpujxirnevmavcd' ,'fzostwrlgekhgujxirnevmaycd' ,'fzostwblgqkhpujxindeimaycd' ,'uzostwblgqshpujxirnevmwycd' ,'fzostwzliqkhpujxirnevmaycu' ,'zzostwbtgqkhpijxirnevmaycd' ,'fzoltwblgqkhpujxinncvmaycd' ,'fzostwblgqkzpujxprnevmayhd' ,'fqostwblgqkhpujrirnevmzycd' ,'jzostwblgqkhpuzxkrnevmaycd' ,'fzostwblgqkhpuwxirszvmaycd' ,'fzostwblgqkhpxjxilnevdaycd' ,'fpmstwblgqkhpujxirnwvmaycd' ,'fzoejwblqqkhpujxirnevmaycd' ,'fzostwblgkshzujxirnevmaycd' ,'fzoatqblgqxhpujxirnevmaycd' ,'fzostwblgpkhpujiipnevmaycd' ,'fzostwblgqghpujgbrnevmaycd' ,'izosowblgqkhpujxirnejmaycd' ,'fzostwblgqthpujxjrnevmalcd' ,'fzovtwbcgqkhpujxicnevmaycd' ,'fzoatwblgqkhpujxidntvmaycd' ,'kzpstwblgqihpujxirnevmaycd' ,'fzosawnlgqkhpujxibnevmaycd' ,'fzostwblgqkhpujxarnevdajcd' ,'bzostwblgqkhpujxvrnevmrycd' ,'fzostwblgqkhpwfxirnevmazcd' ,'fzostwblgqknpujyiqnevmaycd' ,'zzostwblgqkhpujyirneqmaycd' ,'flosiwblgqihpujxirnevmaycd' ,'fzoetwblgqkhxujxirnevmeycd' ,'fznstwbugqkhpujxibnevmaycd' ,'fzbstwblgqkhpudxitnevmaycd' ,'fzostwblgqkhkujxirnexaaycd' ,'fzohthblgqkhpujxiknevmaycd' ,'fzostwblgqkhpujxirnevvayjt' ,'fzostwblggkhpujxirnrvqaycd' ,'fzostwblgqslpujxirnevmaysd' ,'aoostwblgqkhpnjxirnevmaycd' ,'fzostwblgqkhlutxirnevuaycd' ,'fxostwbugqkhpujxirnexmaycd' ,'fzoftwblgqkhpsjxirnevmaywd' ,'fzbstwblgqkhndjxirnevmaycd' ,'fzostwblgqkhpxjxipnlvmaycd' ,'fzostwbloqkhowjxirnevmaycd' ,'fzostwblgqkcpdjxirnevnaycd' ,'vzostiblgqkhpsjxirnevmaycd' ,'fzostwblgqkhazjxirnevmaycg' ,'fzostaklgqkhpujxirnevmaypd' ,'fzostwblgkkhppjxirnevpaycd' ,'izostwblgqkhpujairhevmaycd' ,'fzostwdlgqkhpuqxzrnevmaycd' ,'fzostwblgqkepujxernevmayct' ,'fzostdblgqkhpujxyrnehmaycd' ,'fzostwblgqkhsujxirnenfaycd' ,'fzostwblgqkhpujxifnevmajld' ,'fzostwblgokhpujxirxemmaycd' ,'fzastwblcqkhpujxiruevmaycd' ,'fzostwsxgqkhpuexirnevmaycd' ,'xzosxwblgqkhpujxirnetmaycd' ,'fzostwblgqkhpuexirnevmkccd' ,'fzostwblgqklpujxirnermfycd' ,'fzoetwblgqkhpujxirnehhaycd' ,'ffostwblgvkhpujxirnevmazcd' ,'fcosywblgqkhpujxirnevmaycy' ,'fzmstwblgqkhpujxdrnevmaycl' ,'fpostwblgqahpujxirnqvmaycd' ,'fzostwbmgqkhpulxornevmaycd' ,'fzostwblgqkopujxqrnevmrycd' ,'fzostwblgqkhpujxisnevmjgcd' ,'fzogtwulfqkhpujxirnevmaycd' ,'fzostwalgqkhpcjxirnevmayud' ,'fzosxwblgqkhpujxirnevmasmd' ,'fzostwblgrkowujxirnevmaycd' ,'fzostsblgqkhpujxirnevmsccd' ,'fzostwblgqkhpujxfrnnvmaocd' ,'fzostwblgqkhpujxiynsvkaycd' ,'fzosowblgqkhpwjxirnevmaecd' ,'fzosgwblgfkhpujxyrnevmaycd' ,'fzostsblgzkhpujmirnevmaycd' ,'fzostwblrqkhyuixirnevmaycd' ,'qzostwblgqkhpujxyrnevmvycd' ,'lzostwblgqkhpujxirqevmaymd' ,'fzostwblgqkbpujxirnefbaycd' ,'fzostwblgmihpujxirnevmafcd' ,'fzostmblgqkhpujxirnevmpynd' ,'fzoltwblgqkhpujlihnevmaycd' ,'fzostwblgqkhpujdirneviyycd' ,'fzgspwblgqkhpqjxirnevmaycd' ,'fzostwblgqkhtujkirnevmayld' ,'fjostwblgqkhpagxirnevmaycd' ,'fzpsthblgqkhpuzxirnevmaycd' ,'fzostwblgqkhpuhxzrneymaycd' ,'fzoftwblgqkepujxirnevcaycd' ,'fzostwblgqkbpyjxipnevmaycd' ,'fzostwqlgqkhpujxirjevmayad' ,'fzoxtwblgqkypupxirnevmaycd' ,'fzostwblgqwhpuoxiynevmaycd' ,'fzostwblgqkhpfjkirnevmavcd' ,'fzoqtwblgqkhpujxirnermaycp' ,'fzostwbngxkhpujxirnevmayqd' ,'fzostwqogqkhpcjxirnevmaycd' ,'fzostwblyqkhpujxvrnevmayzd' ,'vzostwblgqkhpujxirnevmlzcd' ,'fzostwblaqkhpujxirnevbajcd' ,'fdostwblgqkhpujxzrnevmayod' ,'fzostxbloqkhpujxirnevmcycd' ,'fzostwbcgqkhpyjxirnegmaycd' ,'fzostwblgqkhpuyxirnzrmaycd' ,'fzustwycgqkhpujxirnevmaycd']
+var input = ['abcdef' ,'bababc' ,'abbcde' ,'abcccd' ,'aabcdd' ,'abcdee' ,'ababab'];
+
+var _ = require('underscore');
+var theseDoubles = 0;
+var theseTriples = 0;
+var totalDoubles = 0;
+var totalTriples = 0;
+var occurrences = {};
+
+function countOccurences (array) {
+
+    array.sort();
+
+    var current = null;
+    var cnt = 0;
+    //loop through letters in current string (sorted)
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] != current) {
+            if (cnt === 2) {
+                occurrences[current] = cnt
+            }
+            if (cnt === 3) {
+                occurrences[current] = cnt
+            }
+            current = array[i];
+            cnt = 1;
+        } else {
+            cnt++;
+        }
+    }
+    if (cnt === 2) {
+        occurrences[current] = cnt
+    }
+    if (cnt === 3) {
+        occurrences[current] = cnt
+    }
+    // now get the distinct values in the object and add to the doubles and triples variables accordingly
+    _.unique(Object.values(occurrences)).forEach(el => {
+        if (el === 2) {
+            theseDoubles +=1
+        } else if (el === 3) {
+            theseTriples +=1
+        }
+    });
+
+    totalDoubles += theseDoubles
+    totalTriples += theseTriples
+    occurrences = {}
+    theseDoubles = 0;
+    theseTriples = 0;
+
+}
+
+for (let i = 0; i < input.length; i++) {
+    const el = input[i];
+    var letters = el.split('')
+    countOccurences(letters); // counts up letter occurrences in the current string
+
+}
+
+var checksum = totalDoubles*totalTriples
+// console.log(`There were ${totalDoubles} doubles and ${totalTriples} triples.`);
+console.log(`Your checksum is ${checksum}`);
+
---
+++ test.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    console.log(totals);
+}, 30000);
+
+setTimeout(function() {
+    console.log('Thinking...');
+}, 5000);
